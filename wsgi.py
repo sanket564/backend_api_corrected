497e0a6d731099e4b99e715032d6d8dc242aaa7b
sanket564 <163655243+sanket564@users.noreply.github.com>
2025-06-16 16:19:15 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,5 @@
-from app import create_app
-
-app = create_app()
+import os
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Required by Render
+    app.run(host="0.0.0.0", port=port)
