ac223c9b0a4eddc4f39e8e0d03f7d5ba0e758a61
sanket564 <163655243+sanket564@users.noreply.github.com>
2025-06-16 16:22:54 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
+from app import create_app  # Import your app factory function
 import os
 
+app = create_app()
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Required by Render
+    port = int(os.environ.get("PORT", 10000))  # Render provides the PORT env variable
     app.run(host="0.0.0.0", port=port)
