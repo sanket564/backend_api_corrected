5d9faf2c0c1dbda25d2c976824436373c503cde9
sanket564 <163655243+sanket564@users.noreply.github.com>
2025-06-20 15:19:54 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,4 +6,4 @@ load_dotenv()
 class Config:
     # SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "16c8733b8d05fa2e4c9649465b8ea78c34ca4ba1f342bc6a84b5d6d553052ed5")
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/attendance_db")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://virupaksh:Virupaksh%401234@cluster0.goirrab.mongodb.net/attendance_db?retryWrites=true&w=majority&appName=Cluster0")
