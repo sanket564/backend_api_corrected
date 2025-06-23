6ec6e9a53694bb7976c91edc83632e0d55715074
sanket564 <163655243+sanket564@users.noreply.github.com>
2025-06-23 19:00:39 +0530

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,6 +2,7 @@ from .auth import auth_bp
 from .attendance import attendance_bp
 from .admin import admin_bp
 from .employee import employee_bp
+from .manager import manager_bp
 
 # List of all blueprints for easy registration
 all_blueprints = [
