42036bc67db8d8b0c7258ef64e3cd2885f599cc8
sanket564 <163655243+sanket564@users.noreply.github.com>
2025-06-24 15:49:43 +0530

    Update manager.py

--- app/routes/manager.py
+++ app/routes/manager.py
@@ -21,7 +21,6 @@ def team_list():
     return jsonify(team), 200
 
 @manager_bp.route("/checkins/pending", methods=["GET"])
-@admin_required()
 @jwt_required()
 def manager_pending_checkins():
     email = get_jwt_identity()
