0096f977714dfec23eda3bcbfc4dabe9a6f67533
sanket564 <163655243+sanket564@users.noreply.github.com>
2025-06-30 18:07:50 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Config:
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "16c8733b8d05fa2e4c9649465b8ea78c34ca4ba1f342bc6a84b5d6d553052ed5")
     MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://virupaksh:Virupaksh%401234@cluster0.goirrab.mongodb.net/attendance_db?retryWrites=true&w=majority&appName=Cluster0")
     # ✅ Outlook SMTP Email Config
-    MAIL_SERVER = 'smtp.office365.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
