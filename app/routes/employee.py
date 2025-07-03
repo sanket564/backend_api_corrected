438658817da9ed433cf0f334984f2317a65e3c42
Sanket Biradar <biradarsanket83@gmail.com>
2025-07-03 14:36:19 +0530

    added import line from dateutil.relativedelta import relativedelta

--- app/routes/employee.py
+++ app/routes/employee.py
@@ -56,6 +56,8 @@ from werkzeug.security import generate_password_hash
 from datetime import datetime
 from app.extensions import mongo
 from ..models.user import get_user_by_email
+from dateutil.relativedelta import relativedelta
+
 
 employee_bp = Blueprint("employee", __name__)  
 
